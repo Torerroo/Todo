3df95f88b9ffaafe287b42420b6e69d484e64a10
Torerroo <Torerro1996@mail.ru>
2023-04-03 18:36:04 +0300

    start

---
+++ .eslintrc.js
@@ -0,0 +1,65 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb-typescript/base'
+  ],
+  ignorePatterns: ["react-app-env.d.ts", ".eslintrc.js"],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    "import/prefer-default-export": 0,
+    'linebreak-style': 0,
+    'no-console': 0,
+    'react/jsx-uses-react': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/prop-types': 0,
+    'import/no-cycle': 0,
+    'import/no-extraneous-dependencies': 0,
+    'react/jsx-no-constructed-context-values': 0,
+    'no-param-reassign': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+  },
+};
