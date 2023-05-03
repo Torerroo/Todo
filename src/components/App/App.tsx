30dd91ec7a1b9fb4244e2ffd22cae25dd44b1838
Torerroo <Torerro1996@mail.ru>
2023-05-03 12:31:47 +0300

    new comp

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,12 @@
 import { AddNewTodo } from '../AddNewTodo/AddNewTodo'
+import { Header } from '../Header/Header'
 import { TodoList } from '../TodoList/TodoList'
+import './App.scss'
 
 export function App() {
   return (
     <div className="App">
+      <Header />
       <AddNewTodo />
       <TodoList />
     </div>
